31e02321982be0ba012b0e0b7e9bc50afcb21436
kolyas238 <kolyas238@gmail.com>
2020-03-21 02:07:03 +0300

    правки по критериям

--- js/modal.js
+++ js/modal.js
@@ -1,47 +1,40 @@
+var link = document.querySelector(".sedona_hotel_search");
+var popup = document.querySelector(".modal_search");
+var dateIn = popup.querySelector("[name=datein]");
+var dateOut = popup.querySelector("[name=dateout]");
+var storage = localStorage.getItem("datein");
+var isStorageSupport = true;
+var storage = "";
 
-  var link = document.querySelector(".sedona_hotel_search");
-
-  var popup = document.querySelector(".modal_search");
-  var form = popup.querySelector("form");
-  var dateIn = popup.querySelector("[name=datein]");
-  var dateOut = popup.querySelector("[name=dateout]");
-
-  var storage = localStorage.getItem("datein");
-
-  var isStorageSupport = true;
-  var storage = "";
-
-  try {
+try {
   storage = localStorage.getItem("datein");
-  } catch (err) {
+} catch (err) {
   isStorageSupport = false;
-  }
+}
 
-  if (popup) {
-    popup.classList.add("modal_hide");
-  }
+if (popup) {
+  popup.classList.add("modal_hide");
+}
 
-  link.addEventListener("click", function (evt) {
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal_error");
+  popup.classList.toggle("modal_hide");
+  dateIn.focus();
+  if (storage) {
+    dateIn.value = storage;
+    dateOut.focus();
+  }
+});
+
+popup.addEventListener("submit", function(evt) {
+  if (!dateIn.value || !dateOut.value) {
     evt.preventDefault();
     popup.classList.remove("modal_error");
-    popup.classList.toggle("modal_hide");
-    dateIn.focus();
-    if (storage) {
-      dateIn.value = storage;
-      dateOut.focus();
-    } else {
-      dateIn.focus();
-    }
-  });
-
-  form.addEventListener("submit", function (evt) {
-if (!dateIn.value || !dateOut.value) {
-   evt.preventDefault();
-   popup.classList.remove("modal_error");
-   popup.offsetWidth = popup.offsetWidth;
+    popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal_error");
   }
-    if (isStorageSupport) {
-      localStorage.setItem("datein", dateIn.value);
+  if (isStorageSupport) {
+    localStorage.setItem("datein", dateIn.value);
   }
-  });
+});
